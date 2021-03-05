faee5d9b2124683065715370bc972281499f339e
Becca <rrdoukas@gmail.com>
2021-03-05 11:34:26 -0600

    updates zip validation

--- js/subscribe.js
+++ js/subscribe.js
@@ -11,11 +11,30 @@ function submitSubscribe(event) {
     birthdate: document.getElementById("birthdate").value,
     zipCode: document.getElementById("zipCode").value,
   };
-  subscribedUsers.push(subscriber);
-  document.querySelector("form").reset(); //clears form
 
-  console.log("The user's information has been added to the subscription list");
-  console.log(subscribedUsers);
+  if (
+    subscriber.firstName === "" ||
+    subscriber.lastName === "" ||
+    subscriber.email === "" ||
+    subscriber.phoneNumber === "" ||
+    subscriber.birthdate === "" ||
+    subscriber.zipCode === ""
+  ) {
+    console.log("This user is unable to subscribe to our newsletter");
+  } else if (
+    subscriber.zipCode.length !== 5 ||
+    typeof subscriber.zipCode !== "number"
+  ) {
+    console.log("The user entered an invalid zipcode");
+  } else {
+    subscribedUsers.push(subscriber);
+    document.querySelector("form").reset(); //clears form
+
+    console.log(
+      "The user's information has been added to the subscription list"
+    );
+    console.log(subscribedUsers);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
